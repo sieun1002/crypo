bbac27a168548b2fea1eb73419879927b82cb2e9
nata20034 <cyber@DESKTOP-J1HK6TQ.localdomain>
2023-10-04 17:40:57 +0900

    ECDH 업데이트 (10/4 수  학교)

--- ECDH.cpp
+++ ECDH.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
@@ -16,6 +18,8 @@ struct EC{
 int mod_inv(int x, int mod);
 void EC_add(EC E, Point *R, Point P, Point Q);
 void EC_double(EC E, Point *R, Point P);
+void bits_inv(vector<int> *bits, int r);
+void EC_mult(EC E, Point *R, Point P, int r);
 
 int main(){
   Point A, B;
@@ -117,5 +121,55 @@ void EC_double(EC E, Point *R, Point P){
       R->y += E.p;
     }
     R->y %= E.p;
+  }
+}
+
+void bits_inv(vector<int> *bits, int r){
+  for(; r>0; r/=2){
+    (*bits).push_back(r%2);
+  }
+} //r을 비트 단위로 변환 후 역순으로 저장. 
+
+////조시은 코드
+// void EC_mult(EC E, Point *R, Point P, int r){
+//   vector<int> bits;
+//   bits_inv(bits, r);
+
+//   R->x = bits.at(0) * P.x;
+//   R->y = bits.at(0) * P.y;
+
+//   for(int i=1; i<bits.size(); i++){
+//     EC_double(E, &R, R);
+//     R->x = bits.at(i) * P.x + R->x;
+//     R->y = bits.at(i) * P.y + R->y;
+//   }
+//  }
+
+//교수님 코드
+void EC_mult(EC E, Point *R, Point P, int r){
+  vector<int> bits;
+  int n;
+
+  if(P.x == 0 && P.y == 0){
+    R->x = 0;
+    R->y = 0;
+    //cout<<"Point of infinity<<endl"
+  }
+  else if(r<=0){
+    cout<<"error"<<endl;
+  }
+  else{
+    bits_inv(&bits, r); //r>0
+    n = bits.size();
+
+    *R = P; //R=b1 * P
+    //R->x = P.x;
+    //R->y = P.y;
+
+    for(; n>1; n--){
+      EC_double(E, R, *R);
+      if(bits.at(n-2)==1)
+        EC_add(E, R, *R, P);
+    }
   }
 }
